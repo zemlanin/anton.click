754c18ec9d22da9ab141b65f26a47a62cc9f4f9a
Anton Verinov <git@zemlan.in>
2018-11-20 21:23:49 +0200

    read routes from `routes/`

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const fs = require("fs");
+const url = require("url");
+const path = require("path");
+
+const ROUTES_DIR = "./routes";
+
+const routes = fs.readdirSync().reduce(
+  (acc, f) => {
+    acc["/" + f] = fs.readFileSync(path.join(ROUTES_DIR, f)).trim();
+    return acc;
+  },
+  {}
+);
+
 module.exports = (req, res) => {
-  res.end(`sup`);
+  res.end(`sup ${url.parse(req.url).pathname}\n${JSON.stringify(routes)}`);
 }
