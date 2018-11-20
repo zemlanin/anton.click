86ace42c203da244e62010d563e4204b6b946726
Anton Verinov <git@zemlan.in>
2018-11-20 21:27:17 +0200

    forgot to pass path

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const path = require("path");
 
 const ROUTES_DIR = "./routes";
 
-const routes = fs.readdirSync().reduce(
-  (acc, f) => {
-    acc["/" + f] = fs.readFileSync(path.join(ROUTES_DIR, f)).trim();
-    return acc;
-  },
-  {}
-);
+const routes = fs.readdirSync(ROUTES_DIR)
+  .reduce(
+    (acc, f) => {
+      acc["/" + f] = fs.readFileSync(path.join(ROUTES_DIR, f)).toString().trim();
+      return acc;
+    },
+    {}
+  );
 
 module.exports = (req, res) => {
   res.end(`sup ${url.parse(req.url).pathname}\n${JSON.stringify(routes)}`);
