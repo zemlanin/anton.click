ca9b77f6193e3576d0e72ac5b1dc9e708a7bb986
Anton Verinov <git@zemlan.in>
2018-11-21 11:30:24 +0200

    prettier

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 const url = require("url");
 
 const routesContext = require.context("./routes", true, /\.js$/);
-const routes = routesContext
-  .keys()
-  .reduce(
-    (acc, f) => {
-      acc[f.match(/^.(.+)\.js$/i)[1]] = routesContext(f);
-      return acc;
-    },
-    {}
-  );
+const routes = routesContext.keys().reduce((acc, f) => {
+  acc[f.match(/^.(.+)\.js$/i)[1]] = routesContext(f);
+  return acc;
+}, {});
 
 module.exports = (req, res) => {
   const { pathname } = url.parse(req.url);
 
   if (!routes[pathname]) {
     console.log(`404 ${pathname}`);
-    res.writeHead(302, { "Location": "/" });
+    res.writeHead(302, { Location: "/" });
     res.end();
     return;
   }
 
   console.log(`302 ${pathname} -> ${routes[pathname]}`);
-  res.writeHead(302, { "Location": routes[pathname] });
+  res.writeHead(302, { Location: routes[pathname] });
   res.end();
-}
+};
