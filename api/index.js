1815f39f8e32d132e4a1292b8815ad8984dc02aa
Anton Verinov <github@zemlan.in>
2023-02-10 20:18:38 +0100

    redirect to webfinger

--- api/index.js
+++ api/index.js
@@ -14,6 +14,15 @@ const routes = fs.readdirSync(ROUTES_DIR).reduce((acc, f) => {
 
 module.exports = (req, res) => {
   let { pathname } = url.parse(req.url);
+  
+  if (pathname === "/.well-known/webfinger") {
+    res.writeHead(302, {
+      Location: new URL(pathname, routes["index"]).toString(),
+      "Cache-Control": `s-maxage=${60 * 60 * 24 * 365}, max-age=0`
+    });
+    res.end();
+    return;
+  }
 
   const segments = pathname.split(/\/+/).filter(Boolean);
 
